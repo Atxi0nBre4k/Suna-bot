d2ab441fac048e7f7925f59fa20d577cc96ce822
Jorge Arturo Copado Luna <aerjorge01@gmail.com>
2022-01-24 16:20:27 -0800

    welcome message

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
+const generateImage = require("./generateImage")
+
 const client = new Discord.Client({
     intents:[
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 
@@ -16,6 +19,16 @@ client.on("messageCreate", (message) => {
     if (message.content == "Copa"){
         message.reply("Ese we es el Caga fuego")
     }
+})
+
+const welcomeChannelID = "835322632181514301"
+
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelID).send({
+        content:`<@${member.id}> Bienvenidx al Servidor de Copadiux! `,
+        files: [img]
+    })            
 })
 
 client.login(process.env.TOKEN);
